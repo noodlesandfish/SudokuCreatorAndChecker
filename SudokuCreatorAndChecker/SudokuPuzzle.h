7ed3a03bf9de85abd7427548ddacd62f50c74a1f
noodlesandfish23 <jhahahn@hotmail.com>
2025-11-17 18:41:00 -0600

    started defining sudoku puzzles and their stream insertion operator overloads

--- SudokuCreatorAndChecker/SudokuPuzzle.h
+++ SudokuCreatorAndChecker/SudokuPuzzle.h
@@ -1,10 +1,54 @@
 #pragma once
+#include <array>
+#include<iostream>
+
 class SudokuPuzzle
 {
-private:
-	int n{}; // size nxn of puzzle
+public:
 	
-		
-	
+	SudokuPuzzle() = delete;
+	SudokuPuzzle(int);
+
 };
 
+class NineByNine : public SudokuPuzzle
+{
+private: 
+	std::array<std::array<int, 9>, 9> theGrid;
+	
+public:
+
+	std::array<std::array<int, 9>, 9> getGrid();
+
+	NineByNine();
+	friend std::ostream& operator << (std::ostream& os, const NineByNine& rhsPuzzle);
+
+};
+
+class FourByFour : public SudokuPuzzle
+{
+private:
+	std::array<std::array<int, 4>, 4> theGrid;
+
+public:
+
+	std::array<std::array<int, 4>, 4> getGrid();
+
+	FourByFour();
+	friend std::ostream& operator << (std::ostream& os, const FourByFour& rhsPuzzle);
+
+};
+
+class SixteenBySixteen : public SudokuPuzzle
+{
+private:
+	std::array<std::array<int, 16>, 16> theGrid;
+
+public: 
+
+	std::array<std::array<int, 16>, 16> getGrid();
+
+	SixteenBySixteen();
+	friend std::ostream& operator << (std::ostream& os, const SixteenBySixteen& rhsPuzzle);
+
+};
